eedbbaf68d04908bc73a05b1a11419b0a54842ad
efenocchi <emanuele.fenocchi@gmail.com>
2024-01-18 13:19:27 +0100

    name refactoring

---
+++ api/llm_chain.py
@@ -0,0 +1,22 @@
+import os
+
+from langchain.chains import LLMChain
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY")
+
+llm = ChatOpenAI(temperature=0)
+prompt = PromptTemplate(
+    input_variables=["product"],
+    template="What is a good name from a company that makes {product}?",
+)
+
+chain_var = LLMChain(llm=llm, prompt=prompt, verbose=True)
+
+if __name__ == "__main__":
+    print(chain_var.run(prompt))
--- api/main.py
+++ api/main.py
@@ -2,4 +2,4 @@ from fastapi import FastAPI
 from langcorn import create_service
 
 
-app: FastAPI = create_service("llm_chain_file:chain_var")
+app: FastAPI = create_service("llm_chain:chain_var")
